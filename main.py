84692fcdf5e6787229ebdf2f32babdc3ce954743
Filipe Campos <filipepcampos24@gmail.com>
2020-09-04 09:24:58 +0100

    Improved game location tracking

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import cv2
 from screen import Screen
+from ai import AI
 import numpy as np
-import keyboard
+
+SCREEN_RESOLUTION = (2560, 1440)
+
+def show_image(img, ai):
+    cv2.putText(img, str(ai.score), 
+            org=(img.shape[0] // 2, 80), 
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale=1, color=0, thickness=3)
+    cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.rectangle(img, (ai.right, ai.top), (ai.right, img.shape[0]-1), (0,255,255))
+    cv2.imshow("Computer Vision", img)
 
 def main():
-    base_frame = None
-    screen = Screen(1920, 1080)
-
+    screen = Screen(SCREEN_RESOLUTION)
+    ai = AI(screen.get())
     while True:
-        img = screen.get_screen()    
-        screen.get_score()
-        T, thresh_img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
-        if cv2.countNonZero(thresh_img) == 0:
-            img = cv2.bitwise_not(img)
-            T, thresh_color = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
-        cv2.imshow("Computer Vision", thresh_img)
+        img = screen.get()
+        ai.step(img)
+        show_image(img, ai)
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        key = cv2.waitKey(50) & 0xFF
+        if key == ord('q'):
             cv2.destroyAllWindows()
             break
+        elif key == ord('s'):
+            screen.locate_game()
+            ai = AI(screen.get())
         
-
 
 if __name__ == "__main__":
     main()
--- screen.py
+++ screen.py
@@ -1,42 +1,54 @@
 from mss import mss
 import cv2
 import numpy as np
-from digit_reader import read
 
 class Screen:
     sct = mss()
     score = 0
-    monitor = {}
-    score_borders = {}
+    monitor, game_area = {}, {}
+    HEIGHT_OFFSET = 10
 
-    def __init__(self, width, height):
-        self.monitor = {'top': 0, 'left': 0, 'width': width, 'height': height}
+
+    def __init__(self, resolution):
+        self.monitor = {'top': 0, 'left': 0, 'width': resolution[0], 'height': resolution[1]}
         self.get_borders()
 
     def get_borders(self):
-        sct_img = self.sct.grab(self.monitor)
-        img = self.convert_img(sct_img, False)
-        print("Please select game area")
+        img = self.get_raw_screen()
+        
         game_r = cv2.selectROI("Select Box", img)
         locs = ["left", "top", "width", "height"]
         for i, loc in enumerate(locs):
             self.monitor[loc] = game_r[i]
-        print("Please select score area")
-        score_r = cv2.selectROI("Select Box", img)
-        for i, loc in enumerate(locs):
-            self.score_borders[loc] = score_r[i]
-        cv2.destroyAllWindows()
-        
-    def convert_img(self, sct_img, convert_to_gray=True):
-        img = np.array(sct_img)
-        return img if not convert_to_gray else cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    def get_screen(self):
-        return self.convert_img(self.sct.grab(self.monitor))
+        self.game_area = self.monitor.copy()
+        cv2.destroyAllWindows()
     
-    def get_score(self):
-        img = self.convert_img(self.sct.grab(self.score_borders))
-        if cv2.countNonZero(cv2.bitwise_not(img)) == 0:
-            self.score += 1
-        cv2.imshow("Score", img)
-        return read(img)
+    def locate_game(self):
+        img = cv2.cvtColor(self.get_raw_screen(), cv2.COLOR_BGR2GRAY)
+        binary_img = (img < 100).astype(int)
+
+        column_sum = binary_img.sum(axis=1)
+        top = np.argmax(column_sum > 0)
+        bot = np.argmax(column_sum)
+        left = np.argmax(binary_img.sum(axis=0) > 10)
+
+        self.game_area['top'] = top + self.monitor["top"]
+        self.game_area['height'] = bot - top - self.HEIGHT_OFFSET
+        self.game_area['left'] = left + self.monitor["left"]
+        self.game_area['width'] = self.monitor['width'] - left
+
+    def convert_img(self, sct_img):
+        img = np.array(sct_img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        T, thresh_img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+        if cv2.countNonZero(thresh_img) == 0:
+            img = cv2.bitwise_not(img)
+            T, thresh_img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+        return thresh_img
+
+    def get_raw_screen(self):
+        return np.array(self.sct.grab(self.monitor))
+
+    def get(self):
+        return self.convert_img(self.sct.grab(self.game_area))
