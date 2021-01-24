f862595ad7061328115cbfa7229a35715dd82a22
Filipe Campos <filipepcampos24@gmail.com>
2021-01-24 17:28:28 +0000

    Code refactor

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import cv2
-from screen import Screen
-from ai import AI
+from ScreenRecorder import ScreenRecorder
+from Display import Display
+from AI import AI
 import numpy as np
 import datetime
 import time
 
-SCREEN_RESOLUTION = (2560, 1440)
-
-def show_image(img, ai, distance, height):
-    """
-    Show an visual representation of the AI's vision
-    """
-    cv2.putText(img, str(ai.score), 
-            org=(img.shape[0] // 2, 80), 
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
-            fontScale=1, color=0, thickness=3)
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.rectangle(img, (ai.right, ai.top), (ai.right, img.shape[0]-1), (0,0,255), 1)
-    cv2.rectangle(img, ai.reference_pixels[0], ai.reference_pixels[1], (255,0,0), 1)
-    if(height):
-        cv2.rectangle(img, (ai.right, height), (ai.right + distance, height), (0, 255, 0), 3)
-    cv2.imshow("Computer Vision", img)
+SCREEN_RESOLUTION = (1920,1080)
 
 def play_once(params, screen):
     """
@@ -29,6 +15,7 @@ def play_once(params, screen):
     ai = AI(params)
     screen.locate_game()
     ai.start(screen.get())
+    display = Display()
 
     with open("log.txt", "a") as file:
         now = datetime.datetime.now()
@@ -39,7 +26,7 @@ def play_once(params, screen):
     while True:
         img = screen.get()
         distance, height = ai.step(img)
-        show_image(img, ai, distance, height)
+        display.show_image(img, ai, distance, height)
 
         if(ai.gameover()):
             break
@@ -56,20 +43,23 @@ def play_once(params, screen):
 
 
 def main():
-    screen = Screen(SCREEN_RESOLUTION)
-    cv2.imshow("Computer Vision", screen.get())
+    screen_recorder = ScreenRecorder(SCREEN_RESOLUTION)
+    cv2.imshow("Computer Vision", screen_recorder.get())
     print("Please keep the game window focused")
     print("Game will start in 5 seconds")
-    for i in range(5):
-        time.sleep(1)
-        print(".", end="", flush=True)
+    wait_animation()
 
     params = {"jump_dist": 100,
             "jump_dist_delta": 45,
             "jump_time": 0.2,
             "crouch_time": 1}
-    play_once(params, screen)
+    play_once(params, screen_recorder)
   
+
+def wait_animation():
+    for i in range(5):
+        time.sleep(1)
+        print(".", end="", flush=True)
 
 if __name__ == "__main__":
     main()
